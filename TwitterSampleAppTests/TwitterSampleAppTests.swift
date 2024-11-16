7acab3090a52944382d6fb9004c87c551e3a7674
Hombo-Yasutaka <y.honbo@genbasupport.com>
2024-11-16 19:59:24 +0900

    Initial Commit

---
+++ TwitterSampleAppTests/TwitterSampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSampleAppTests.swift
+//  TwitterSampleAppTests
+//
+//  Created by 本坊康孝 on 2024/11/16.
+//
+
+import Testing
+@testable import TwitterSampleApp
+
+struct TwitterSampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
