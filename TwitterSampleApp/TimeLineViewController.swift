b09cb68d861ce8871a36500d9253e097c2e4598b
Hombo-Yasutaka <y.honbo@genbasupport.com>
2024-11-16 21:29:16 +0900

    UITableViewによるUI作成

---
+++ TwitterSampleApp/TimeLineViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TimeLineViewController.swift
+//  TwitterSampleApp
+//
+//  Created by 本坊康孝 on 2024/11/16.
+//
+
+import Foundation
+import UIKit
+
+class TimeLineViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        print("TimeLineViewControllerが表示されました")
+    }
+}
