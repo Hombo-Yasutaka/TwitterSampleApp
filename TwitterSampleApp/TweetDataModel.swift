467c7ac1251855d4ba181dd8fcf244c6a4a8cfa9
Hombo-Yasutaka <y.honbo@genbasupport.com>
2024-11-18 19:27:11 +0900

    UITableViewにダミーデータを表示

---
+++ TwitterSampleApp/TweetDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  TweetDataModel.swift
+//  TwitterSampleApp
+//
+//  Created by 本坊康孝 on 2024/11/18.
+//
+
+import Foundation
+
+struct TweetDataModel {
+    var name: String
+    var content: String
+}
