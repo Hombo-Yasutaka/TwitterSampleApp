03c1f159fd62303d285acada9a9f4fc505b29588
Hombo-Yasutaka <y.honbo@genbasupport.com>
2024-11-20 17:06:24 +0900

    ツイート画面の表示

---
+++ TwitterSampleApp/EditorViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EditorViewController.swift
+//  TwitterSampleApp
+//
+//  Created by 本坊康孝 on 2024/11/20.
+//
+
+import UIKit
+
+class EditorViewController: UIViewController {
+
+    @IBOutlet weak var tweetButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureTweetButton()
+    }
+
+
+    @IBAction func tappedCancelButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    func configureTweetButton() {
+        tweetButton.layer.cornerRadius = tweetButton.bounds.height / 2
+    }
+}
